f4eb946d5fd5d48d38fe7a0838b43e1337aed857
Rey Vincent Canlas <94040143+vinxentience@users.noreply.github.com>
2022-03-09 12:51:59 +0800

    Add files via upload

---
+++ LCD/task_4/task_4.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.print("Arduino");
+  delay(3000);
+
+  lcd.setCursor(2,1);
+  lcd.print("LCD with Arduino");
+  delay(3000);
+
+  lcd.clear();
+
+  lcd.blink();
+  delay(4000);
+  lcd.setCursor(7,1);
+}
